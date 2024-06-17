315b9ea026296fc3884647f242fe8714527ed7fe
TomBrown114514 <tomb0968@outlook.com>
2024-06-17 15:58:08 +0800

    Setup Spring MVC and Encoding Filter configuration.- Implement EncodingFilter to ensure UTF-8 encoding for requests and responses. - Introduce SpringMvcConfig to configure Spring MVC aspects such as default servlet handling and view resolvers. - These changes set up the foundation for Spring MVC application with proper encoding handling.

---
+++ src/main/java/com/itheima/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package com.itheima.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan({"com.itheima.controller"})
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/admin/", ".jsp");
+    }
+}
