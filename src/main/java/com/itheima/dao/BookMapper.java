9b904e64fe9aee2d46f9d86fe8d6fe79e4f687f2
TomBrown114514 <tomb0968@outlook.com>
2024-06-18 08:25:01 +0800

    Add Book class and BookMapper interface for database operations.
    - Implement the Book class with properties and methods for book management.- Create the BookMapper interface with a selectNewBooks method for fetching books from the database.
    - Use annotations to map database columns to class properties for easy data retrieval and manipulation.

---
+++ src/main/java/com/itheima/dao/BookMapper.java
@@ -0,0 +1,26 @@
+package com.itheima.dao;
+
+import com.github.pagehelper.Page;
+import com.itheima.domain.Book;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+public interface BookMapper {
+    @Select("select * from book where book_status != '3' order by book_uploadtime desc ")
+    @Results(id = "bookMap", value = {
+            @Result(id = true, column = "book_id", property = "id"),
+            @Result(column = "book_name", property = "name"),
+            @Result(column = "book_isbn", property = "isbn"),
+            @Result(column = "book_press", property = "press"),
+            @Result(column = "book_author", property = "author"),
+            @Result(column = "book_pagination", property = "pagination"),
+            @Result(column = "book_price", property = "price"),
+            @Result(column = "book_uploadtime", property = "uploadtime"),
+            @Result(column = "book_status", property = "status"),
+            @Result(column = "book_borrower", property = "borrower"),
+            @Result(column = "book_borrowtime", property = "borrowtime"),
+            @Result(column = "book_returntime", property = "returntime")
+    })
+    Page<Book> selectNewBooks();
+}
